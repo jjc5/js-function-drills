ec77bcd80ec084beb0969e717f383ae2210ff9bb
Jason C <jayingmantis.555@gmail.com>
2021-08-03 15:18:41 -0500

    palindrome complete

--- solutions.js
+++ solutions.js
@@ -5,3 +5,15 @@
 //Return actually returns a value when console.log prints it in the console for debugging or checking work.
 
 /*********************************************************************/
+
+// function palindrome(str) {
+//  let splitted = str.split("");
+//  let reversed = splitted.reverse("");
+//  let joined = reversed.join("");
+//  return joined.toLowerCase().replace(/[^0-9a-z]/gi, '') == str.toLowerCase().replace(/[^0-9a-z]/gi, '')
+// }
+
+// console.log(palindrome('Racecar'))
+//I HAD TO SEARCH FOR HELP ON THIS ONE
+
+/***********************************************************************/
