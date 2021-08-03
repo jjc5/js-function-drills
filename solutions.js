d3bfe25494f8e02ade194e22e979d7642644c5ae
Jason C <jayingmantis.555@gmail.com>
2021-08-03 15:16:47 -0500

    verbal questions complete

---
+++ solutions.js
@@ -0,0 +1,7 @@
+//1.
+//Parameters are the names passed in a function and arguments are the actual values.
+
+//2.
+//Return actually returns a value when console.log prints it in the console for debugging or checking work.
+
+/*********************************************************************/
